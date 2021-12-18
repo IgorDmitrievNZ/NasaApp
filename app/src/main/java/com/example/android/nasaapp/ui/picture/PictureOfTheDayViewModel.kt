8150904a42bd15ca4c2c623703ba9159370586b8
Igor <idmitrievnz@gmail.com>
2021-12-18 21:50:03 +1300

    hw1 update Removed old functionality for hd photo Added functionality to chips for today's and yesterday's photos

--- app/src/main/java/com/example/android/nasaapp/ui/picture/PictureOfTheDayViewModel.kt
+++ app/src/main/java/com/example/android/nasaapp/ui/picture/PictureOfTheDayViewModel.kt
@@ -11,13 +11,20 @@ import com.example.android.nasaapp.ui.AppState
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PictureOfTheDayViewModel(
     private val liveDataForViewToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val liveDataForYesterdayViewToObserve: MutableLiveData<AppState> = MutableLiveData(),
     private val retrofitImpl: PictureOfTheDayRetrofitImpl = PictureOfTheDayRetrofitImpl()
 ) : ViewModel() {
     fun getData(): LiveData<AppState> {
         return liveDataForViewToObserve
+    }
+
+    fun getYesterdayData(): LiveData<AppState> {
+        return liveDataForYesterdayViewToObserve
     }
 
     fun sendServerRequest() {
@@ -26,11 +33,13 @@ class PictureOfTheDayViewModel(
         if (apiKey.isBlank()) {
             liveDataForViewToObserve.value = AppState.Error(Throwable("wrong key"))
         } else {
-            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(callback)
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(callbackToday)
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey, getDaysAgo(1))
+                .enqueue(callbackYesterday)
         }
     }
 
-    private val callback = object : Callback<PictureOfTheDayResponseData> {
+    private val callbackToday = object : Callback<PictureOfTheDayResponseData> {
         override fun onResponse(
             call: Call<PictureOfTheDayResponseData>,
             response: Response<PictureOfTheDayResponseData>
@@ -47,5 +56,32 @@ class PictureOfTheDayViewModel(
             Log.e("ERROR", "callback error: in PictureOfTheDayViewModel ${call.request()}")
             liveDataForViewToObserve.value = AppState.Error(Throwable())
         }
+    }
+
+    private val callbackYesterday = object : Callback<PictureOfTheDayResponseData> {
+        override fun onResponse(
+            call: Call<PictureOfTheDayResponseData>,
+            response: Response<PictureOfTheDayResponseData>
+        ) {
+            if (response.isSuccessful && response.body() != null) {
+                liveDataForYesterdayViewToObserve.value = AppState.Success(response.body()!!)
+            } else {
+                Log.e("ERROR", "callback error: in PictureOfTheDayViewModel ${call.request()}")
+                liveDataForYesterdayViewToObserve.value = AppState.Error(Throwable())
+            }
+        }
+
+        override fun onFailure(call: Call<PictureOfTheDayResponseData>, t: Throwable) {
+            Log.e("ERROR", "callback error: in PictureOfTheDayViewModel ${call.request()}")
+            liveDataForYesterdayViewToObserve.value = AppState.Error(Throwable())
+        }
+    }
+
+    private fun getDaysAgo(daysAgo: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_YEAR, -daysAgo)
+        val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+        val format = formatter.format(calendar.time)
+        return format.toString()
     }
 }
