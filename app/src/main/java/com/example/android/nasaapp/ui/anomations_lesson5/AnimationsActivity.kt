71c50cd47f72fd0fcf17925faf9e071f4aab6b2e
Igor <idmitrievnz@gmail.com>
2022-01-04 16:31:35 +1300

    lesson 5. Dealt with TransitionSet

--- app/src/main/java/com/example/android/nasaapp/ui/anomations_lesson5/AnimationsActivity.kt
+++ app/src/main/java/com/example/android/nasaapp/ui/anomations_lesson5/AnimationsActivity.kt
@@ -3,8 +3,10 @@ package com.example.android.nasaapp.ui.anomations_lesson5
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.transition.AutoTransition
+import androidx.transition.ChangeBounds
+import androidx.transition.Fade
 import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.example.android.nasaapp.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
@@ -19,14 +21,21 @@ class AnimationsActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.button.setOnClickListener {
             isTextViewVisible = !isTextViewVisible
-            val transition = AutoTransition()
+
+//            val transition = AutoTransition()
+//            transition.duration = 2000
+//            /* аналогичные команды */
+//            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
+//            TransitionManager.beginDelayedTransition(binding.transitionsContainer,AutoTransition())
+//            /* аналогичная команда, но время анимации изменено на 2 сек */
+
+            var transition = TransitionSet()// исчезновение
+            transition.addTransition(Fade())
+            transition.addTransition(ChangeBounds())
+//            transition.ordering = TransitionSet.ORDERING_SEQUENTIAL // указали последовательное выполнение
             transition.duration = 2000
-            /* аналогичные команды */
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,AutoTransition())
-            /* аналогичная команда, но время анимации изменено на 2 сек */
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer,transition)
-            binding.text.visibility = if(isTextViewVisible) View.VISIBLE else View.GONE
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transition)
+            binding.text.visibility = if (isTextViewVisible) View.VISIBLE else View.GONE
         }
     }
 }
