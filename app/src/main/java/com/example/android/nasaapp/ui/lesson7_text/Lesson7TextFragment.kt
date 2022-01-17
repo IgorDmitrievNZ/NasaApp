70f2be011718cab393162eeaf638696ee6d9c6d7
Igor <idmitrievnz@gmail.com>
2022-01-17 17:33:57 +1300

    lesson 7. Work with TextView without calling setText, through channels: TextView.BufferType.NORMAL(SpannedText)< TextView.BufferType.SPANNABLE(SpannableString)< TextView.BufferType.EDITABLE(SpannableStringBuilder)

--- app/src/main/java/com/example/android/nasaapp/ui/lesson7_text/Lesson7TextFragment.kt
+++ app/src/main/java/com/example/android/nasaapp/ui/lesson7_text/Lesson7TextFragment.kt
@@ -6,10 +6,13 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.style.BulletSpan
+import android.text.style.ForegroundColorSpan
 import android.text.style.ImageSpan
+import android.text.style.QuoteSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.example.android.nasaapp.BaseFragment
 import com.example.android.nasaapp.R
@@ -29,6 +32,7 @@ class Lesson7TextFragment : BaseFragment<FragmentLeson7TextBinding>() {
 
         val spannableMutable = SpannableStringBuilder("My \n text \n text \nbullet one \nbullet two")
         val spannableUnMutable = SpannableString("My text \nbullet one \nbullet two")
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             spannableMutable.setSpan(
@@ -55,5 +59,58 @@ class Lesson7TextFragment : BaseFragment<FragmentLeson7TextBinding>() {
         }
 
         binding.textView.text = spannableMutable
+
+        val text = binding.textView2.text
+        val spannableStart = SpannableStringBuilder(text)
+        binding.textView2.setText(spannableStart,TextView.BufferType.EDITABLE)
+        val spannable = binding.textView2.text as SpannableStringBuilder
+        initSpans(spannable)
+
+    }
+
+    private fun initSpans(spannable: SpannableStringBuilder) {
+        spannable.setSpan(
+            ForegroundColorSpan(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.colorAccent
+                )
+            ),
+            0, 10, Spannable.SPAN_EXCLUSIVE_INCLUSIVE // FIXME EXCLUSIVE_INCLUSIVE
+        )
+        spannable.insert(0, "1")
+        spannable.insert(10, "\n")
+        spannable.insert(20, "\n")
+
+        val q: QuoteSpan
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            q = QuoteSpan(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.colorAccent
+                ),
+                20,
+                40
+            )
+        } else {
+            q = QuoteSpan(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.colorAccent
+                )
+            )
+        }
+        spannable.setSpan(
+            q, 0, 20, Spannable.SPAN_EXCLUSIVE_INCLUSIVE // FIXME EXCLUSIVE_INCLUSIVE
+        )
+        val qq = QuoteSpan(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.colorAccent
+            )
+        )
+        spannable.setSpan(
+            qq, 10, 19, Spannable.SPAN_EXCLUSIVE_INCLUSIVE // FIXME EXCLUSIVE_INCLUSIVE
+        )
     }
 }
